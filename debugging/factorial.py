fdc85c6ea2b0718d2295d6c584bd2b71808a7cfe
matthew25072005 <8855@holbertonstudents.com>
2024-09-03 09:50:18 -0300

    task 00

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -2,22 +2,11 @@
 import sys
 
 def factorial(n):
-    if n < 0:
-        return "Error: El número debe ser no negativo"
-    elif not isinstance(n, int):
-        return "Error: El número debe ser un entero"
-    else:
-        result = 1
-        while n > 1:
-            result *= n
-            n = n - 1
-        return result
+    result = 1
+    while n > 1:
+        result *= n
+        n -= 1  # Decrementa n en cada iteración
+    return result
 
-if len(sys.argv) > 1:
-    try:
-        f = factorial(int(sys.argv[1]))
-        print(f)
-    except ValueError:
-        print("Error: El argumento debe ser un número entero")
-else:
-    print("Error: Debe proporcionar un argumento al ejecutar el script")
+f = factorial(int(sys.argv[1]))
+print(f)
